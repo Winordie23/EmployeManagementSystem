d3765e2032fbb736ce4cb53c278201a57ecbc443
Yeab23 <88987557+Winordie23@users.noreply.github.com>
2022-01-24 01:34:25 -0800

    Delete New folder directory

--- New folder/FxRegistration/src/main/java/com/example/fxregistration/confirmer.java
+++
@@ -1,8 +0,0 @@
-package com.example.fxregistration;
-
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-
-public interface confirmer extends EventHandler<ActionEvent> {
-    void confirmer(ActionEvent event);
-}
